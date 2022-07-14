1552a66320b95a0a9290ea9d68352d9c508004ce
Hussam <hussamalbahadli@gmail.com>
2022-07-14 18:40:06 +0300

    completed the challenge 001

--- bin/challenge.dart
+++ bin/challenge.dart
@@ -1,1 +1,6 @@
+import 'package:challenge/challenge001.dart';
 
+void main(List<String> args) {
+  TellMeJoke tellMeJoke = TellMeJoke();
+  tellMeJoke.tellMeJoke();
+}
---
+++ lib/challenge001.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+class TellMeJoke {
+  aim() {
+    String aim = '\nWelcome to challenge 001.'
+        '\nChallenge Descraption, Write a program that will display a joke'
+        '\nDon’t display the punchline until the reader hits the enter key.'
+        '\nExtension : display the punchline in a different colour '
+        '\nPrior Knowledge Needed : [How to output information to the screen]';
+
+    print('\x1B[31m $aim');
+  }
+
+  tellMeJoke() {
+    aim();
+    print('\x1B[35m \nWhy the fish lived in the water ?');
+    String inputKey = stdin.readLineSync()!;
+    if (inputKey == '') {
+      print('\x1B[32m \nTo take a shower.. (:\n');
+    }
+  }
+}
